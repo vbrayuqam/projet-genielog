65067cdecb915f2bb9b18eb036fa4b21f7e6d77f
Alex <qc23bastionZ@gmail.com>
2022-07-24 16:39:14 -0400

    ajout du connecteur BD

--- demo/src/main/java/CoucheDonnees/ConnecteurBD.java
+++ demo/src/main/java/CoucheDonnees/ConnecteurBD.java
@@ -1,14 +1,28 @@
 package CoucheDonnees;
-// import java.sql.DriverManager;
 
-// import sqlite3;
+import java.sql.*;
+
+//import sqlite3;
 
 
-// public class connecteurDB {
+public class ConnecteurBD {
     
-//     Connection conn = null;
+     Connection conn = null;
+
+     public static Connection ConnectionDB(){
+
+         try{
+             Class.forName("org.sqlite.JDBC");
+             Connection conn = DriverManager.getConnection("jdbc:sqlite:src/bd/BD.db");
+             System.out.println("Connexion reussie");
+             return conn;
+         }
+         catch(Exception e){
+             System.out.println("Connexion échouée");
+             return null;
+         }
+     }
 
 
-//     conn = DriverManager.getConnection("jbdc:sqlite3:bd.db");
 
-// }
+}
