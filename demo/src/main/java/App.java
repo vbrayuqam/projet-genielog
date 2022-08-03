92c13f57fda778945701ead757676f3fb087876e
lpoirier <poirier.lucas@courrier.uqam.ca>
2022-08-03 14:08:00 -0400

    Page Connexion

--- demo/src/main/java/App.java
+++ demo/src/main/java/App.java
@@ -1,10 +1,13 @@
 import CoucheDonnees.*;
+import CoucheLogique.*;
 import CouchePresentation.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        SystemeDossier systemeDossier = new SystemeDossier();
+        PageConnexion pageConnexion = new PageConnexion(systemeDossier);
         ApplicationMedecin appMed = new ApplicationMedecin();
     }
 }
