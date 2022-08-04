5f3fcf4d9e87c250ee2f282f4b5d7969519a6eb2
Vincent Bray <vincentbrayqc@gmail.com>
2022-08-04 11:51:11 -0400

    ajout lib json

--- demo/src/main/java/App.java
+++ demo/src/main/java/App.java
@@ -1,10 +1,25 @@
 import CoucheDonnees.*;
+import CoucheLogique.*;
 import CouchePresentation.*;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import net.sf.json.JsonConfig;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        ApplicationMedecin appMed = new ApplicationMedecin();
+        Dossier test = new Dossier();
+        Patient pat = new Patient();
+        test.setPatient(pat);
+
+        JSONObject js = JSONObject.fromObject(test);
+
+        System.out.println(js.toString());
+
+
+
+        // ApplicationMedecin appMed = new ApplicationMedecin();
     }
 }
+
